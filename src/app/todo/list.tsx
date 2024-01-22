addf059e5d4ab0f60f522c3bc30a3018fc3795e9
hhao99 <hhao99@gmail.com>
2024-01-22 09:44:46 -0600

    update the table

--- src/app/todo/list.tsx
+++ src/app/todo/list.tsx
@@ -2,47 +2,74 @@ import { useStore } from './store'
 import type { ITodo } from '@/types/todo'
 import type { IAppState } from '@/types/appstate'
 
+// next ui
+import {
+    Table,
+    TableHeader,
+    TableColumn,
+    TableBody,
+    TableRow,
+    TableCell
+} from '@nextui-org/react'
+
+
+
 
 export function TodoItem({todo}: {todo:ITodo}) {
     const { remove, toggle } = useStore( (state:IAppState) => state)
 
+   
 
     return (
-      <tr>
-        <td>{todo.id}</td>
-        <td>{todo.task}</td>
-        <td>
+      <TableRow key={todo.id}>
+        <TableCell>{todo.id}</TableCell>
+        <TableCell>{todo.task}</TableCell>
+        <TableCell>
             <span onClick={e => toggle(todo.id)}>
             <input type='checkbox' checked={todo.complete} />
                 {todo.complete? 'ok': 'open'}
                 
             </span>
-        </td>
-        <td><button onClick={e=> remove(todo.id)}>remove</button></td>
-      </tr>
+        </TableCell>
+        <TableCell><button onClick={e=> remove(todo.id)}>remove</button></TableCell>
+      </TableRow>
       
     );
   }
   
 export  function TodoList() {
     const { list } = useStore( (state:IAppState)=> state)
+    const columns = [
+        { 
+            key: 'id',
+            label: "Id"
+        },
+        { 
+            key: 'task',
+            label: "Task"
+        },
+        { 
+            key: 'status',
+            label: "Status"
+        },
+        { 
+            key: 'action',
+            label: "Action"
+        }
+    ]
   return (
     <div>
         <h1>Todo List</h1>
         <h5>{list.length} of task(s) to be done:</h5>
-        <table className='table-auto'>
-            <thead>
-                <th>ID</th>
-                <th>Task</th>
-                <th>status</th>
-                <th>action</th>
-            </thead>
-            <tbody>
-            {list && list.map( (todo: ITodo) => <TodoItem key={todo.id} todo={todo}/>  )}
-
-            </tbody>
+        <Table >
+            <TableHeader columns={columns}>
+                { c=> <TableColumn key={c.key}>{c.label}</TableColumn> }
+            </TableHeader>
+            <TableBody items={list}>
+                {(item: ITodo) => <TodoItem key={item.id} todo={item}/>  }
+            </TableBody>
             
-          </table>
+        </Table>
     </div>
     
   );
