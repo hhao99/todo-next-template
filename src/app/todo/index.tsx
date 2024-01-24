1fbaf15148d2188760a66f938ee9bc335dec3bd1
hhao99 <hhao99@gmail.com>
2024-01-24 00:35:00 -0600

    complete nextui

--- src/app/todo/index.tsx
+++ src/app/todo/index.tsx
@@ -9,6 +9,15 @@ import {
   Popover,
   PopoverTrigger
 } from '@nextui-org/react'
+
+const NewIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+</svg>
+
+  )
+}
 export default function TodoApp() {
   return (
     <main>
@@ -17,7 +26,7 @@ export default function TodoApp() {
         <div> </div>
         <Popover placement='left'>
           <PopoverTrigger>
-            <Button>new task</Button>
+            <Button><NewIcon /></Button>
           </PopoverTrigger>
           <PopoverContent>
             <AddTodo />
--- src/app/todo/list.tsx
+++ src/app/todo/list.tsx
@@ -37,11 +37,31 @@ export  function TodoList() {
     ]
 
 
+    const RemoveIcon = ()=> {
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+
+      )
+    }
+
+    const DoneIcon = ()=>  (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+  </svg>
+  )
+    const OpenIcon = () => (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+  </svg>
+  )
+
   const RemoveAlerDialog = ({id}:{id:string})=> {
     const {isOpen, onOpen, onOpenChange} = useDisclosure()
     return (
       <>
-        <Button onPress={onOpen}>remove</Button>
+        <Button onPress={onOpen}>
+          <RemoveIcon className='w-4 h-4' />
+        </Button>
         <Modal aria-label='remove dialog'
           isOpen={isOpen} 
           onOpenChange={onOpenChange}>
@@ -58,7 +78,7 @@ export  function TodoList() {
                   Cancel
                 </Button>
                 <Button color="danger" onPress={e=> {remove(id); onClose()}}>
-                  Remove!
+                  Confirm
                 </Button>
               </ModalFooter>
               </> 
@@ -72,8 +92,10 @@ export  function TodoList() {
   return (
     <div>
         <h1>Todo List</h1>
-        <h5>{list.length} of task(s) to be done:</h5>
-        <Table aria-label='todo list table'
+        <h5>{list.length} task(s) to be done:</h5>
+        <Table 
+          removeWrapper
+          aria-label='todo list table'
           isStriped
           selectionMode='multiple'
           selectedKeys={selectedKeys}
@@ -97,7 +119,7 @@ export  function TodoList() {
                   <Switch 
                     onChange={e=> toggle(item.id)} 
                     isSelected={item.complete}>
-                      {item.complete?'ok':'open'}
+                      {item.complete?<DoneIcon />:<OpenIcon />}
                       </Switch>
                   </TableCell>
                 <TableCell className='w-1/5'>
