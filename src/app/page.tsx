87ae747a5ec0dd4d06a5d088aaafff492320f3e6
Eric Hao <hhao99@gmail.com>
2024-01-24 07:39:36 -0600

    update homepage update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-
+import { Link } from 'next'
 export default function Home() {
   return (
     <main>
       <h1>Todo Demo App</h1>
+      <h3><Link href='/table'>The Table demo</Link></h3>
+      <h3><Link href='/todo'>The Table demo</Link></h3>
     </main>
     
   );
