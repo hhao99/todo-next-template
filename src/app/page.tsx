bff79d0df7964060b287a0af2ee46686bfbd2243
hhao99 <hhao99@gmail.com>
2024-01-24 07:44:03 -0600

    update the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Link } from 'next'
+import Link  from 'next/link'
 export default function Home() {
   return (
-    <main>
-      <h1>Todo Demo App</h1>
+    <main className='m-4 p-4'>
+      <h1 className='text-3xl text-center'>Todo Demo App</h1>
       <h3><Link href='/table'>The Table demo</Link></h3>
-      <h3><Link href='/todo'>The Table demo</Link></h3>
+      <h3><Link href='/todo'>The Todo demo</Link></h3>
     </main>
     
   );
